7e1595636d75c2b2e12be30d3695112be88afe9b
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 16:01:05 +0000

    Refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,14 +2,18 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-
+  
+  def angular
+      @angular_portfolio_items = Portfolio.angular
+  end
+  
  def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
-  end
+ end
 
  def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(params.require(portfolios_params))
 
    respond_to do |format|
       if @portfolio_item.save
@@ -18,23 +22,23 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
-  end
+ end
 
  def edit
     @portfolio_item = Portfolio.find(params[:id])
-  end
+ end
 
  def update
     @portfolio_item = Portfolio.find(params[:id])
 
    respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolios_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
-  end
+ end
 
  def show
     @portfolio_item = Portfolio.find(params[:id])
@@ -51,6 +55,17 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
-  end
+ end
 
+    private
+    
+    def portfolio_params
+        params.require(:portfolio).permit(:title, 
+                                          :subtitle, 
+                                          :body, 
+                                          technologies_attributes: [:name]
+                                          )
+                                          
+
+    end
 end
