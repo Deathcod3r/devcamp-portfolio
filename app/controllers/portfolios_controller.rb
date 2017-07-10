e71afae307fe114177ae38f20b124b19c5645e1a
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 16:43:58 +0000

    Implemented partials

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,72 +1,73 @@
 class PortfoliosController < ApplicationController
   layout 'portfolio'
+  
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def angular
-      @angular_portfolio_items = Portfolio.angular
+    @angular_portfolio_items = Portfolio.angular
   end
-  
- def new
+
+  def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
- end
+  end
 
- def create
-    @portfolio_item = Portfolio.new(params.require(portfolios_params))
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
 
-   respond_to do |format|
+    respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
- end
+  end
 
- def edit
-    @portfolio_item = Portfolio.find(params[:id])
- end
-
- def update
+  def edit
     @portfolio_item = Portfolio.find(params[:id])
 
-   respond_to do |format|
-      if @portfolio_item.update(portfolios_params)
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
- end
+  end
 
- def show
+  def show
     @portfolio_item = Portfolio.find(params[:id])
- end
+  end
 
- def destroy
+  def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
 
-   # Destroy/delete the record
+    # Destroy/delete the record
     @portfolio_item.destroy
 
-   # Redirect
+    # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
- end
+  end
 
-    private
-    
-    def portfolio_params
-        params.require(:portfolio).permit(:title, 
-                                          :subtitle, 
-                                          :body, 
-                                          technologies_attributes: [:name]
-                                          )
-                                          
+  private
 
-    end
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
+  end
+
 end
