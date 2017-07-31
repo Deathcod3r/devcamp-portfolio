1a5355a41641c23d69e01230aca10823eb55066b
Jordan Hudgens <jordan@devcamp.com>
2017-07-31 15:39:15 +0000

    implemented partials and pagination

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -1,15 +1,12 @@
 module DefaultPageContent
   extend ActiveSupport::Concern
-  
+
   included do
     before_filter :set_page_defaults
   end
-  
+
   def set_page_defaults
     @page_title = "Devcamp Portfolio | My Portfolio Website"
-    @seo_keywords = "Jeremy Cunningham portfolio"
+    @seo_keywords = "Jordan Hudgens portfolio"
   end
 end
-
-
-
