4ad652ed7e5ccf18e33229fa08021b9984db102d
Jordan Hudgens <jordan@devcamp.com>
2017-08-10 17:09:52 +0000

    integrated helper methode for notifacations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,16 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+  
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    
+    if alert
+      alert_generator
+    end  
+  end
+  
+  def alert_generator msg
+    js add_gritter(msg, title: "Jeremy Cunningham's Portfolio", sticky: false)
+  end
 end
